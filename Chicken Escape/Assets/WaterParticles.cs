9d15a8924cd7e7a3261fcd6b104124ee6413076b
Brandon Bell <brandondgbell@gmail.com>
2016-03-24 18:08:56 -0700

    Added water Particles + updated Terrain
    The terrain now has trees and different textures as well as grass.

---
+++ Chicken Escape/Assets/WaterParticles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterParticles : MonoBehaviour
+{
+    public ParticleSystem particle;
+    public GameObject splash;
+
+    private GameObject thisSplash;
+    private GameObject player;
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    void OnTriggerEnter(Collider collide)
+    {
+        if (collide.gameObject == player)
+        {
+            //REMOVE THE COMMENTS AND FIX FOR WATER FOOTSTEPS
+       //     thisSplash = (GameObject)Instantiate(splash, collide.transform.position, collide.transform.rotation);
+       //     thisSplash.SetActive(true);
+            particle.Play();
+            particle.gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider collide)
+    {
+        particle.Stop();
+    }
+}
